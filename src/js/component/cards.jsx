b52fb633df59ee3b8a13ad6695149e30864767c1
cpgravina <74896127+cpgravina@users.noreply.github.com>
2024-06-20 10:24:22 +0000

    adding components

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+
+export const Cards = () => {
+	return (
+<>
+<div className="container">
+  <div className="row row-cols-1 row-cols-sm-1 row-cols-xm-1 row-cols-lg-5">
+    <div className="col">
+      <div className="card px-3 border-0" style={{width: '18rem' }}>
+        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="#" className="btn btn-primary">Call to action!</a>
+        </div>
+    </div>
+    </div>
+    <div className="col">
+    <div className="card px-3 border-0" style={{width: '18rem' }}>
+        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="#" className="btn btn-primary">Call to action!</a>
+        </div>
+    </div>
+    </div>
+    <div className="col">
+    <div className="card px-3 border-0" style={{width: '18rem' }}>
+        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="#" className="btn btn-primary">Call to action!</a>
+        </div>
+    </div>
+    </div>
+    <div className="col">
+    <div className="card px-3 border-0" style={{width: '18rem' }}>
+        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="#" className="btn btn-primary">Call to action!</a>
+        </div>
+    </div>
+    </div>
+    <div className="col">
+    <div className="card px-3 border-0" style={{width: '18rem' }}>
+        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="#" className="btn btn-primary">Call to action!</a>
+        </div>
+    </div>
+    </div>
+  </div>
+</div>
+</>
+	);
+};
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Footer = () => {
+	return (
+<>
+<footer className="py-3 my-4 text-white bg-dark">
+    <p className="text-center text-body-secondary">copyrights © Your website 2018</p>
+  </footer>
+</>
+	);
+};
